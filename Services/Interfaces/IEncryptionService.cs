0431139e7b854cb5cf0c8cc618bfa541ca4632cc
Глеб Гращенков <deadrater@protonmail.com>
2022-05-23 13:49:40 +0300

    Добавлена функция хэширования

---
+++ Services/Interfaces/IEncryptionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_ORG_SQLite_RGR_2022.Services.Interfaces
+{
+    public interface IEncryptionService : IDisposable
+    {
+        string ComputeSha256Hash(string s);
+    }
+}
