abaebbd771b2914117e6ef16d3a3f5e15db12521
Глеб Гращенков <deadrater@protonmail.com>
2022-05-24 16:42:51 +0300

    Added CustomExceptions

---
+++ Services/CustomExceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_ORG_SQLite_RGR_2022.Services
+{
+    public class CustomExceptions
+    {
+        string ExName { get; set; }
+        string ExDescription { get; set; }
+
+        public void ThrowCustomException(string ExName, string ExDescription)
+        {
+            MessageBox.Show(ExDescription,ExName,MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+
+        public void ThrowNewException(Exception ex)
+        {
+            ExName = $"ERROR! {ex.Source}";
+            ExDescription = ex.Message;
+
+            ThrowCustomException(ExName, ExDescription);
+        }
+    }
+}
